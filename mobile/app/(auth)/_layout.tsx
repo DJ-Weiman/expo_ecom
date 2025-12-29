7608c6fd095083ad5f1984d76fc110246d1d96ff
DJ-Weiman <dionweiman1998@gmail.com>
2025-12-29 21:09:01 +0530

    Init for Mobile

---
+++ mobile/app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Redirect, Stack } from "expo-router";
+
+export default function AuthRoutesLayout() {
+    const isSignedIn = false
+    
+    if(isSignedIn){
+        return <Redirect href={'/'} />
+    }
+
+    return <Stack screenOptions={{headerShown: false}} />
+}
